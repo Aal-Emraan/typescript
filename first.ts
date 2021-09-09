ab0fd87aca98df90654c46e01933c315c98b9ea3
Aal Emraan <developer.aalemraan@gmail.com>
2021-09-09 23:59:27 +0600

    practiced

--- first.ts
+++ first.ts
@@ -12,3 +12,19 @@ function addNumbers(x: number, y: number): number{
 }
 
 addNumbers(5, 40);
+
+const salaries: number[] = [43895,467576,78,45,345,35];
+salaries.push(39284);
+const friends: string[] = ['name', 'all', 'emraan', 'aal'];
+friends.push('boy');
+friends[0] = 'boss';
+salaries[1] = 489;
+
+// object declaration in typescript
+type values = {name: string, age: number, salary: number};
+const person: values = {name:'aal emraan', age: 23, salary: 34000}
+const person2: {name: string, age: number, company: string} = {
+    name: 'aal emraan',
+    age: 23,
+    company: 'google'
+}
