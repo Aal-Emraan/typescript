08c7dd64c00e2f8556cb96b5593167aff9790bef
Aal Emraan <developer.aalemraan@gmail.com>
2021-09-09 23:22:09 +0600

    type learned

---
+++ first.ts
@@ -0,0 +1,14 @@
+const number = 6;
+let myName = 'aal emraan';
+console.log('hello typescript', number,myName);
+
+let yourName: string = 'cool dude';
+let num: number = 50;
+let validation: boolean = true;
+
+function addNumbers(x: number, y: number): number{
+    const added = x + y;
+    return added
+}
+
+addNumbers(5, 40);
---
+++ js/first.js
@@ -0,0 +1,12 @@
+"use strict";
+const number = 6;
+let myName = 'aal emraan';
+console.log('hello typescript', number, myName);
+let yourName = 'cool dude';
+let num = 50;
+let validation = true;
+function addNumbers(x, y) {
+    const added = x + y;
+    return added;
+}
+addNumbers(5, 40);
